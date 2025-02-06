03d2a542bb881950a2dcc339ccd33061c44e8006
vertskater <vertskater@gmx.at>
2025-02-06 13:58:40 +0100

    add filter for projects

--- src/components/projects.ts
+++ src/components/projects.ts
@@ -1,6 +1,8 @@
 const projects = [
     {
+        id: 1,
         title: "Bookshelf",
+        category: 'Frontend',
         description:
             "Eine einfaceh CRUD Applikation die Firebase als Datenbank und Benutzer-Authentifizierung nutzt.",
         github: "https://github.com/vertskater/bookshelf-fs",
@@ -9,7 +11,9 @@ const projects = [
         techStack: ['/icons/firebase.svg', '/icons/css.svg', '/icons/typescript.svg', '/icons/react.svg']
     },
     {
+        id: 2,
         title: "React Memory Game",
+        category: 'Frontend',
         description:
             "Ein einfaches Merkspiel mit Highscore Anzeige und Punktestand Zähler.",
         github: "https://github.com/vertskater/memory-game/tree/main",
@@ -18,7 +22,9 @@ const projects = [
         techStack: ['/icons/react.svg', 'icons/javascript.svg', 'icons/sass.svg']
     },
     {
+        id: 3,
         title: "React Shopping Cart",
+        category: 'Frontend',
         description:
             "Ein kleines Webshop-Frontend mit Suchfunktion und Warenkorb",
         github: "https://github.com/vertskater/shopping-cart",
@@ -27,7 +33,9 @@ const projects = [
         techStack: ['/icons/react.svg', '/icons/javascript.svg', '/icons/css.svg', '/icons/react-router.svg']
     },
     {
+        id: 4,
         title: "Webshop PHP",
+        category: 'Backend',
         description: "Ein Webshop mit PHP und MariaDB als Datenbank.",
         github: "https://github.com/vertskater/php-webshop",
         live: "",
@@ -35,7 +43,19 @@ const projects = [
         techStack: ['/icons/php.svg', '/icons/html.svg', '/icons/css.svg', '/icons/mariadb.svg']
     },
     {
+        id: 5,
+        title: "WordPress Theme",
+        category: 'Backend/Frontend',
+        description: "Ein WordPress-Theme zum Darstellen von Kursen im Schulungskontext.",
+        github: "https://github.com/vertskater/wp_theme",
+        live: "",
+        img: "/img/wordpress.png",
+        techStack: ['/icons/php.svg', '/icons/html.svg', '/icons/sass.svg', '/icons/css.svg', '/icons/mariadb.svg', '/icons/wordpress.svg']
+    },
+    {
+        id: 6,
         title: "My-Drive",
+        category: 'Frontend/Backend',
         description:
             "Eine Applikation mit Nodejs/Express/PostgreSQL zum Erstellen von Verzeichnissen und hochladen von Dateien.",
         github: "https://github.com/vertskater/my-drive",
@@ -44,7 +64,9 @@ const projects = [
         techStack: ['/icons/nodejs.svg', "/icons/express.svg", '/icons/postgresql.svg', '/icons/prisma.svg', "/icons/javascript.svg"]
     },
     {
+        id: 7,
         title: "Blog API",
+        category: 'Backend',
         description:
             "Eine API zum Abrufen von Blogbeiträgen und Kommentaren. Umgesetzt mit Nodejs/Express/PostgreSQL.",
         github: "https://github.com/vertskater/blog-api",
@@ -53,7 +75,9 @@ const projects = [
         techStack: ['/icons/nodejs.svg', "/icons/express.svg", '/icons/postgresql.svg', '/icons/prisma.svg', '/icons/jwt.svg']
     },
     {
+        id: 8,
         title: "Realtime Chat",
+        category: 'Backend',
         description:
             "Ein Echtzeitchat mit Socket.io, Express als Backend und React als Frontend.",
         github: "https://github.com/vertskater/chat-app-backend",
